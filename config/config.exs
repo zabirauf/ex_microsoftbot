ec95146121792a8a182cd1a5186e1b87b7989d3d
Patrick Goddi <patrick.goddi@gmail.com>
2017-09-04 23:54:58 -0700

    MS Bot Framework V3.1 support (#13)
    * modified token_manager.ex to use env variables and changed config to include v3.1 url strings for bot-conector-bot communication
    
    moved hardcoded url strings to config file to allow overriding them in a local config
    
    added output of experimental version to verify version in testing
    
    updated mix.exs openid_valid_keys_url env var
    
    fixed issue with still using constant name for auth_api_endpoint
    
    removed version debug message
    
    * updated test and dev config files to point to v3.1 urls
    
    * added defaults for scope and api_auth URLs so not required in config file, thus more consistent with version 1.0, values can be overridden with config file
    
    * removed api_auth and scope values from config file to be more consistent with 1.0 version

--- config/config.exs
+++ config/config.exs
@@ -29,7 +29,7 @@ use Mix.Config
 #
 config :ex_microsoftbot,
   endpoint: "https://api.botframework.com",
-  openid_valid_keys_url: "https://api.aps.skype.com/v1/.well-known/openidconfiguration",
+  openid_valid_keys_url: "https://login.botframework.com/v1/.well-known/openidconfiguration",
   issuer_claim: "https://api.botframework.com",
   audience_claim: Application.get_env(:ex_microsoftbot, :app_id),
   disable_token_validation: false
