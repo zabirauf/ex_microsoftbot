7786ac481150cad12decdae0d5d8e170de132963
Sean Omlor <somlor@eml.cc>
2018-04-06 15:34:07 -0400

    sweep sweep

--- lib/token_manager.ex
+++ lib/token_manager.ex
@@ -4,14 +4,14 @@ defmodule ExMicrosoftBot.TokenManager do
   Microsoft bot framework and also is responsible for refreshing the token
   before it expires.
   """
+
   use ExMicrosoftBot.RefreshableAgent
+  alias ExMicrosoftBot.{Client, Models}
 
-  alias ExMicrosoftBot.Models
-  alias ExMicrosoftBot.Client
+  @auth_api_endpoint "https://login.microsoftonline.com/botframework.com/oauth2/v2.0/token"
+  @scope "https://api.botframework.com/.default"
 
-  ################################################
-  ##### Functions to interact with GenServer #####
-  ################################################
+  # Public API
 
   @doc """
   Get the token that can be used to authorize calls to Microsoft Bot Framework
@@ -22,17 +22,17 @@ defmodule ExMicrosoftBot.TokenManager do
     token
   end
 
-  #######################################
-  ##### Refreshable Agent Callbacks #####
-  #######################################
+  # Refreshable Agent Callbacks
 
   @default_time_gap_to_refresh_in_seconds 500
+
   @doc """
   Refresh the token by making a service call and then scheduling a message to
   this GenServer before token expires so that it can be refreshed
   """
   def get_refreshed_state(%Models.AuthData{} = auth_data, old_state) do
-    Application.get_env(:ex_microsoftbot, :using_bot_emulator)
+    :ex_microsoftbot
+    |> Application.get_env(:using_bot_emulator)
     |> get_refreshed_state(auth_data, old_state)
   end
 
@@ -40,30 +40,28 @@ defmodule ExMicrosoftBot.TokenManager do
   The time to refresh which is taken from the response of the token
   """
   def time_to_refresh_after_in_seconds(%{expiry_in_seconds: expiry_in_seconds}) do
-    (expiry_in_seconds - @default_time_gap_to_refresh_in_seconds)
+    expiry_in_seconds - @default_time_gap_to_refresh_in_seconds
   end
 
-  ###############################
-  ####### Helper functions ######
-  ###############################
+  # Private
 
   defp get_refreshed_state(true, %Models.AuthData{} = _auth_data, _old_state) do
     %{token: "TestToken", expiry_in_seconds: 36000}
   end
 
   defp get_refreshed_state(_, %Models.AuthData{} = auth_data, _old_state) do
-    refresh_token(auth_data)
-    |> validate_token
+    auth_data
+    |> refresh_token()
+    |> validate_token()
   end
 
   defp validate_token(%{token: token} = token_response) do
     # TODO: See what other checks are needed to verify the JWT
     with jwt <- JOSE.JWT.peek_payload(token),
-         true <- contains_valid_app_id_claim?(jwt)
-         do
-           true
-         else
-           result -> raise "Error validating token. Result: #{inspect(result)}"
+         true <- contains_valid_app_id_claim?(jwt) do
+      true
+    else
+      result -> raise "Error validating token. Result: #{inspect(result)}"
     end
 
     token_response
@@ -73,9 +71,12 @@ defmodule ExMicrosoftBot.TokenManager do
     contains_valid_app_id_claim?(Application.get_env(:ex_microsoftbot, :app_id), jwt)
   end
 
-  defp contains_valid_app_id_claim?(expected_app_id, %JOSE.JWT{fields: %{"appid" => expected_app_id}}), do: true
-  defp contains_valid_app_id_claim?(_, %JOSE.JWT{}), do: false
+  defp contains_valid_app_id_claim?(expected_app_id, %JOSE.JWT{
+         fields: %{"appid" => expected_app_id}
+       }),
+       do: true
 
+  defp contains_valid_app_id_claim?(_, %JOSE.JWT{}), do: false
 
   defp refresh_token(%Models.AuthData{} = auth_data) do
     {:ok, token_response} = get_token_from_service(auth_data)
@@ -86,29 +87,31 @@ defmodule ExMicrosoftBot.TokenManager do
     }
   end
 
-  @auth_api_endpoint "https://login.microsoftonline.com/botframework.com/oauth2/v2.0/token"
-  @scope "https://api.botframework.com/.default"
   defp get_token_from_service(%Models.AuthData{app_id: app_id, app_password: app_password}) do
-    auth_api_endpoint = Application.get_env(:ex_microsoftbot, :auth_api_endpoint) || @auth_api_endpoint
+    auth_api_endpoint =
+      Application.get_env(:ex_microsoftbot, :auth_api_endpoint) || @auth_api_endpoint
+
     scope = Application.get_env(:ex_microsoftbot, :scope) || @scope
 
-    body = [
-      dummy_param: "dummy", # In testing the first param was not detected by the API hence adding a dummy param
-      grant_type: "client_credentials",
-      client_id: app_id,
-      client_secret: app_password,
-      scope: scope
-    ] |> convert_to_post_params_string
+    body =
+      convert_to_post_params_string(
+        # In testing the first param was not detected by the API hence adding a dummy param
+        dummy_param: "dummy",
+        grant_type: "client_credentials",
+        client_id: app_id,
+        client_secret: app_password,
+        scope: scope
+      )
 
-
-    HTTPotion.post(auth_api_endpoint, [body: Poison.encode!(body)])
-    |> Client.deserialize_response(&(Poison.decode!(&1, as: %{})))
+    auth_api_endpoint
+    |> HTTPotion.post(body: Poison.encode!(body))
+    |> Client.deserialize_response(&Poison.decode!(&1, as: %{}))
   end
 
   defp convert_to_post_params_string(params) do
-    Enum.reduce(params, [], fn ({k, v}, acc) -> ["#{k}=#{URI.encode_www_form(v)}" | acc] end)
-    |> Enum.reverse
+    params
+    |> Enum.reduce([], fn {k, v}, acc -> ["#{k}=#{URI.encode_www_form(v)}" | acc] end)
+    |> Enum.reverse()
     |> Enum.join("&")
   end
-
 end
