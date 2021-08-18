9fef716c9a6276921a3ed67fc1815390ac818efb
João Ferreira <jmnsferreira@gmail.com>
2021-08-18 12:11:07 +0100

    Adds support for clientInfo and mention Entities

--- lib/models/entity.ex
+++ lib/models/entity.ex
@@ -3,17 +3,43 @@ defmodule ExMicrosoftBot.Models.Entity do
   Microsoft bot entity structure
   """
 
+  alias ExMicrosoftBot.Models.Entity.Mention
+
   @derive [Poison.Encoder]
-  defstruct [:type, :name, :supportsDisplay]
+  defstruct [
+    :type,
+
+    # type == "?"
+    :name,
+    :supportsDisplay,
+
+    # type == "clientInfo"
+    :country,
+    :locale,
+    :platform,
+    :timezone,
+
+    # type == "mention"
+    :mentioned,
+    :text
+  ]
 
   @type t :: %ExMicrosoftBot.Models.Entity{
-    type: String.t,
-    name: String.t,
-    supportsDisplay: boolean,
-  }
+          type: String.t(),
+          name: String.t() | nil,
+          supportsDisplay: boolean() | nil,
+          country: String.t() | nil,
+          locale: String.t() | nil,
+          platform: String.t() | nil,
+          timezone: String.t() | nil,
+          mentioned: Mention.t() | nil,
+          text: String.t() | nil
+        }
 
   @doc false
   def decoding_map() do
-    %ExMicrosoftBot.Models.Entity{}
+    %ExMicrosoftBot.Models.Entity{
+      mentioned: Mention.decoding_map()
+    }
   end
 end
